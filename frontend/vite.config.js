17be6c52bc1b3632790b64f117e698d3efa2a708
HarshvardhanPandey2003 <harshvardhanpandey2003@gmail.com>
2025-09-25 09:57:30 +0530

    Updating Dev

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
   },
   // Add this for local development
   server: {
-    port: 5173,
+    port: 3000,
     proxy: {
       '/api': {
         target: 'http://localhost:5000',
