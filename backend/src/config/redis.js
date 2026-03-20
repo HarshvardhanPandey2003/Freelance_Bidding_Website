a552366aa3a508d16bc3128bb1ceb3b9ca7e3cf1
HarshvardhanPandey2003 <harshvardhanpandey2003@gmail.com>
2026-03-20 18:24:14 +0530

    Updating Redis Config

--- backend/src/config/redis.js
+++ backend/src/config/redis.js
@@ -1,5 +1,3 @@
-//src/condig/redis.js
-// src/config/redis.js (IMPROVED VERSION)
 // src/config/redis.js
 import { createClient } from 'redis';
 
@@ -17,28 +15,3 @@ export default redisClient;
 
 
 
-
-
-// import { createCluster, createClient } from 'redis';
-
-// // Better detection: use NODE_ENV instead of URL parsing
-// const isCluster = process.env.NODE_ENV === 'prod';
-
-// const redisClient = isCluster 
-//   ? createCluster({
-//       rootNodes: [{
-//         url: process.env.REDIS_URI || 'redis://redis-cluster-0.redis-cluster:6379'
-//       }]
-//     })
-//   : createClient({
-//       url: process.env.REDIS_URI || 'redis://localhost:6379'
-//     });
-
-// redisClient.on('error', (err) => console.error('Redis Client Error:', err));
-
-// (async () => {
-//   await redisClient.connect();
-//   console.log(`Connected to Redis in ${isCluster ? 'CLUSTER' : 'SINGLE'} mode`);
-// })();
-
-// export default redisClient;
